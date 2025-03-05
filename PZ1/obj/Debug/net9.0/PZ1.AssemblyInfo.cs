c5620faf1ed0d21c57022ace9bc4a906e887870b
jekbe <patrykrusakowicz@gmail.com>
2025-03-05 17:10:18 +0100

    done

--- PZ1/obj/Debug/net9.0/PZ1.AssemblyInfo.cs
+++ PZ1/obj/Debug/net9.0/PZ1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PZ1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6a6a19f6c746c48d7c592c189d93c0e2d7cdc83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7bac1ec342df0fe15752634d280f77310bd0b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PZ1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PZ1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
